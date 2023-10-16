fe05509a864f4621a51dbc0735c75483d1c8942b
joonsuk12 <96754803+joonsuk12@users.noreply.github.com>
2023-10-16 17:08:06 +0900

    :pencil2: main: Update dbModule.py

--- flask_barbell/dbModule.py
+++ flask_barbell/dbModule.py
@@ -5,11 +5,11 @@ class Database():
     def __init__(self):
         # mysql connection 연결
         self.conn = pymysql.connect(
-            host='localhost',
-            user='root',
-            password='4214',
-            db='letmeseeDB',
-            charset='utf8'
+            host='',
+            user='',
+            password='',
+            db='',
+            charset=''
         )
         # Dictionary cursor 생성
         self.cur = self.conn.cursor(pymysql.cursors.DictCursor)  # 다른 형태의 커서
